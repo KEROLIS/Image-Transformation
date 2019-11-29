211380d1b063bf1b5e7f14761e5f733f5e16f178
KEROLIS <kerlisyoussif12@gmail.com>
2019-11-30 00:16:21 +0200

    first commit

---
+++ uiuc/HSLAPixel.h
@@ -0,0 +1,23 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+
+  // Put your HSLAPixel class definition here.
+class HSLAPixel	{
+public:
+double h,s,l,a;
+};
+  // (Remember to end it with a semicolon!)
+
+
+}
